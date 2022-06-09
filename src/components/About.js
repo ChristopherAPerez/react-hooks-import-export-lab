b5f16652aec303dc689b605151acba71c37bed01
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-06-09 10:30:21 -0400

    commit tree df8d852105f5b2754ac991b54c2d5230f2315bd2

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { image as image1 } from "/Users/chickngamer/react-hooks-import-export-lab/src/data/user.js";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={image1} alt="I made this" />
     </div>
   );
 }
+
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { username as username1, city as city1 } from "/Users/chickngamer/react-hooks-import-export-lab/src/data/user.js";
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {username1} is a Web Developer from {city1}
       </h1>
     </div>
   );
 }
+
+export default Home;
